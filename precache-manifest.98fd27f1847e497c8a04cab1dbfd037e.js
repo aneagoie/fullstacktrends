a304761b82d1b205bbf4ae204e2ea3cc9e22414f
aneagoie <aneagoie1@gmail.com>
2018-12-16 11:08:12 -0600

    Updates

---
+++ precache-manifest.98fd27f1847e497c8a04cab1dbfd037e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6c566ba97ce260a27c1",
+    "url": "/fullstacktrends/static/js/runtime~main.a6c566ba.js"
+  },
+  {
+    "revision": "d64a08d26cd70556c68d",
+    "url": "/fullstacktrends/static/js/main.d64a08d2.chunk.js"
+  },
+  {
+    "revision": "5b265051865492349dc6",
+    "url": "/fullstacktrends/static/js/1.5b265051.chunk.js"
+  },
+  {
+    "revision": "d64a08d26cd70556c68d",
+    "url": "/fullstacktrends/static/css/main.4582c14c.chunk.css"
+  },
+  {
+    "revision": "8ab313a44fd1abf9f8bf4942035c70b9",
+    "url": "/fullstacktrends/index.html"
+  }
+];
