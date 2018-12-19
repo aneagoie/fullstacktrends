53c31ac2f9d0a1d247c2153ef3bb318577daf3b0
aneagoie <aneagoie1@gmail.com>
2018-12-19 11:54:32 -0600

    adding data source section

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ class App extends Component {
                     </div>
                 </div>
                 <Newsletter />
-                <Data />
+                <Data cData={this.state.cData}/>
                 <Footer />
             </div>
         );
