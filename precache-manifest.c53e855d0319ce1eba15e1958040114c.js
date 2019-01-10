84c79db9f47a331d709b445f22cb4adcadf86489
aneagoie <aneagoie1@gmail.com>
2019-01-10 08:54:50 -0500

    Updates

---
+++ precache-manifest.c53e855d0319ce1eba15e1958040114c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cbbd78ce179d2e2e4de435f37ade66a",
+    "url": "/coding_challenge-13/static/media/parallax.4cbbd78c.svg"
+  },
+  {
+    "revision": "ebb669c47397a5a058fcb49bd4c09f0b",
+    "url": "/coding_challenge-13/static/media/logo1alt.ebb669c4.psd"
+  },
+  {
+    "revision": "b740f4640d2e2c904757d0ffaa378b84",
+    "url": "/coding_challenge-13/static/media/logo.b740f464.psd"
+  },
+  {
+    "revision": "3f23df8c64386f23ada57e5c0c5d4b5f",
+    "url": "/coding_challenge-13/static/media/bg-whitewave-desktop.3f23df8c.svg"
+  },
+  {
+    "revision": "3e3206c5f1e9618387aa0dc88188be48",
+    "url": "/coding_challenge-13/static/media/bg-wave-desktop.3e3206c5.svg"
+  },
+  {
+    "revision": "78d08d1ad98d4dab53eb",
+    "url": "/coding_challenge-13/static/js/runtime~main.78d08d1a.js"
+  },
+  {
+    "revision": "90be5101855e172bc74e",
+    "url": "/coding_challenge-13/static/js/main.90be5101.chunk.js"
+  },
+  {
+    "revision": "5a710a42596f51c16a55",
+    "url": "/coding_challenge-13/static/js/1.5a710a42.chunk.js"
+  },
+  {
+    "revision": "90be5101855e172bc74e",
+    "url": "/coding_challenge-13/static/css/main.cf2af411.chunk.css"
+  },
+  {
+    "revision": "45882c9b37b392e2662255ebbd7a679d",
+    "url": "/coding_challenge-13/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b01922c94027fb184d98399e13ae6948.js"
+  "/coding_challenge-13/precache-manifest.c53e855d0319ce1eba15e1958040114c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/coding_challenge-13/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
